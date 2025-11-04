654d3561cb25e701184d51938d7f11cae51d4928
kunjpateljs <kunjpatelx@gmail.com>
2025-11-04 16:06:40 -0400

    fix: allow Jest to transform chai (fix ES module error)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/dist/', '/e2e/'],
-  testMatch: ['**/tests/**/*.test.ts']  // Only Jest tests
+  testMatch: ['**/tests/**/*.test.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!chai)/'  // ← TRANSFORM CHAI ONLY
+  ]
 };
